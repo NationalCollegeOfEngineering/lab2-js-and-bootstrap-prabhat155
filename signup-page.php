3bb457f7845eb2a66ab2596b6744e79ac7404caa
prabhat155 <92683212+prabhat155@users.noreply.github.com>
2024-06-08 11:02:15 +0545

    remember_me_removed

--- signup-page.php
+++ signup-page.php
@@ -296,12 +296,6 @@
       <label for="floatingPassword">Confirm Password</label>
     </div>
 
-    <div class="form-check text-start my-3">
-      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Remember me
-      </label>
-    </div>
     <button class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
     <p class="mt-5 mb-3 text-body-secondary">Already have an account? <a href="login-page.php">Login</a></p>
     <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
